005c548b7f9ac7bc740045db523e7019184badd7
Jose <jose.antonio.umeres@gmail.com>
2022-07-26 20:52:35 -0500

    Added the calendar component

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,46 @@
+
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { addHours, format, parse, startOfWeek, getDay } from 'date-fns'
+import enUS from 'date-fns/locale/en-US'
 import { Navbar } from "../"
 
+const locales = {
+  'en-US': enUS,
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+const events = [{
+  title: 'Un evento',
+  notes: 'Hay que comprar el pastel',
+  start: new Date(),
+  end: addHours(new Date(), 3),
+  bgColor: "#fafafa",
+  user: {
+    _id: '123',
+    name: 'Jose'
+  }
+}]
 
 export const CalendarPage = () => {
   return (
     <>
       <Navbar />
+
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80px)' }}
+      />
     </>
   )
 }
