80b3fb4a2ae5bf06aa09637aa6f68f1288a8de39
Jose <jose.antonio.umeres@gmail.com>
2022-07-28 20:56:24 -0500

    Added a modal to show the selected event

--- src/calendar/index.js
+++ src/calendar/index.js
@@ -1,3 +1,5 @@
-export * from './components/Navbar';
 export * from './components/CalendarEvent';
+export * from './components/CalendarModal';
+export * from './components/Navbar';
+
 export * from './pages/CalendarPage';
--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,10 @@
-
+import { useState } from 'react';
 import { Calendar } from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { addHours } from 'date-fns'
-import { Navbar, CalendarEvent } from "../"
+import { Navbar, CalendarEvent, CalendarModal } from "../"
 import { localizer, getMessagesES } from '../../helpers';
-import { useState } from 'react';
+
 
 
 
@@ -73,6 +73,9 @@ export const CalendarPage = () => {
         onSelectEvent={ onSelect }
         onView={ onViewChanged }
       />
+
+      <CalendarModal>
+      </CalendarModal>
     </>
   )
 }
