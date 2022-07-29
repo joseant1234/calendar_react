3217b5c86e5d6c7c09c1982db6f824af9b13c815
Jose <jose.antonio.umeres@gmail.com>
2022-07-29 09:46:20 -0500

    Added onSubmit method

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -1,4 +1,4 @@
-import { addHours } from "date-fns";
+import { addHours, differenceInSeconds } from "date-fns";
 import { useState } from "react";
 import Modal from "react-modal";
 import DatePicker, { registerLocale } from "react-datepicker";
@@ -49,6 +49,16 @@ export const CalendarModal = () => {
         setIsOpen(false);
     }
 
+    const onSubmit = (event) => {
+        event.preventDefault();
+        const difference = differenceInSeconds(formValues.end, formValues.start);
+        if (isNaN(difference) || difference <= 0) {
+            return;
+        }
+
+        if (formValues.title.length <= 0) return;
+    }
+
     return (
         <Modal
             isOpen={ isOpen }
@@ -60,7 +70,7 @@ export const CalendarModal = () => {
         >
             <h1> Nuevo evento </h1>
             <hr />
-            <form className="container">
+            <form className="container" onSubmit={onSubmit}>
 
                 <div className="form-group mb-2">
                     <label>Fecha y hora inicio</label>
