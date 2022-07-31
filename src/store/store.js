e2ce133194c521f7c79189216af595acf9461e15
Jose <jose.antonio.umeres@gmail.com>
2022-07-31 09:17:21 -0500

    Added a middleware to avoid checking the serialization of a date

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { uiSlice, calendarSlice } from "./";
 
+// serializableCheck en false es para q no revise q se pueda serializar la fecha
 export const store = configureStore({
     reducer: {
         calendar: calendarSlice.reducer,
         ui: uiSlice.reducer,
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false
+    })
 })
