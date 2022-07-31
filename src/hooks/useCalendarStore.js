b702d165ba33121d11fb6971b721af99c5d868c8
Jose <jose.antonio.umeres@gmail.com>
2022-07-31 09:02:14 -0500

    Added a method to edit an event

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { onAddNewEvent, onSetActiveEvent } from "../store/calendar/calendarSlice";
+import { onAddNewEvent, onSetActiveEvent, onUpdateEvent } from "../store/calendar/calendarSlice";
 
 export const useCalendarStore = () => {
 
@@ -14,10 +14,10 @@ export const useCalendarStore = () => {
         dispatch(onSetActiveEvent(calendarEvent));
     }
 
-    // otra forma de hacer en lugar de un thunk
+    // otra forma de hacer en lugar de un thunk (simula un thunk)
     const startSavingEvent = async(calendarEvent) => {
         if (calendarEvent._id) {
-
+            dispatch(onUpdateEvent({...calendarEvent}));
         } else {
             dispatch(onAddNewEvent({...calendarEvent, _id: new Date().getTime() }));
         }
