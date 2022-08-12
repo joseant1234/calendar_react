1fcb8011e1f338b9b00858ece4ce13d43c9b4906
Jose <jose.antonio.umeres@gmail.com>
2022-08-12 12:17:56 -0500

    Added an interceptor and checkAuthToken function to keep the authentication status

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -41,6 +41,20 @@ export const useAuthStore = () => {
         }
     }
 
+    const checkAuthToken = async() => {
+        const token = localStorage.getItem('token');
+        if (!token) return  dispatch(onLogout());
+        try {
+            const { data } = await calendarApi.get('auth/renew');
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('token-init-date', new Date().getTime());
+            dispatch(onLogin({ name: data.name, uid: data.uid }));
+        } catch (error) {
+            localStorage.clear();
+            dispatch(onLogout());
+        }
+    }
+
     return {
         //* Properties
         status,
@@ -49,5 +63,6 @@ export const useAuthStore = () => {
         //* Methods
         startLogin,
         startRegister,
+        checkAuthToken,
     }
 }
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,25 @@
+import { useEffect } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { LoginPage } from "../auth";
 import { CalendarPage } from "../calendar";
+import { useAuthStore } from "../hooks";
 
 
 export const AppRouter = () => {
 
-  const authStatus = 'not-authenticated';
+  // const authStatus = 'not-authenticated';
+  const { status:authStatus, checkAuthToken } = useAuthStore();
+
+  useEffect(() => {
+    checkAuthToken();
+  }, []);
+
+
+  if (authStatus === 'checking') {
+    return (
+      <h3>Cargando...</h3>
+    )
+  }
 
   return (
     <Routes>
