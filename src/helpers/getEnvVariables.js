c7b998b2d943b8d6ac37bf564433c8190d674bf1
Jose <jose.antonio.umeres@gmail.com>
2022-08-14 09:20:15 -0500

    Fixed getEnvVariables function to build

--- src/helpers/getEnvVariables.js
+++ src/helpers/getEnvVariables.js
@@ -1,6 +1,10 @@
 export const getEnvVariables = () => {
-    import.meta.env;
+    // en el build esto ocasiona error con el vite, se tiene que una por una
+    // import.meta.env;
+    // return {
+    //     ...import.meta.env
+    // };
     return {
-        ...import.meta.env
-    };
+        VITE_API_URL: import.meta.env.VITE_API_URL,
+    }
 }
