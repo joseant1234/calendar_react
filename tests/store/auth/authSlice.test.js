05b33c351bfb0c243927ead64c9d5916a507e9f1
Jose <jose.antonio.umeres@gmail.com>
2022-08-16 12:55:23 -0500

    Added tests to authSlice

---
+++ tests/store/auth/authSlice.test.js
@@ -0,0 +1,45 @@
+import { authSlice, clearErrorMessage, onLogin, onLogout } from "../../../src/store";
+import { authenticatedState, initialState } from "../fixtures/authStates";
+import { testUserCredentials } from "../fixtures/testUser";
+
+describe('Pruebas en authSlice', () => {
+    test('debe de regresar el estado inicial', () => {
+        expect(authSlice.getInitialState()).toEqual(initialState);
+    });
+
+    test('debe de realizar un login', () => {
+        const state = authSlice.reducer(initialState, onLogin(testUserCredentials));
+        expect(state).toEqual({
+            status: 'authenticated',
+            user: testUserCredentials,
+            errorMessage: undefined,
+        });
+    });
+
+    test('debe de realizar el logout', () => {
+        const state = authSlice.reducer(authenticatedState, onLogout());
+        expect(state).toEqual({
+            status: 'not-authenticated',
+            user: {},
+            errorMessage: undefined,
+        });
+    });
+
+    test('debe de realizar el logout con error', () => {
+        const errorMessage = 'Credenciales no válidadas'
+        const state = authSlice.reducer(authenticatedState, onLogout(errorMessage));
+        expect(state).toEqual({
+            status: 'not-authenticated',
+            user: {},
+            errorMessage,
+        });
+    });
+
+    test('debe de limpiar el mensaje de error', () => {
+        const errorMessage = 'Credenciales no válidadas'
+        const state = authSlice.reducer(authenticatedState, onLogout(errorMessage));
+        const newState = authSlice.reducer(state, clearErrorMessage());
+
+        expect(newState.errorMessage).toBe(undefined);
+    });
+})
---
+++ tests/store/fixtures/testUser.js
@@ -0,0 +1,6 @@
+export const testUserCredentials = {
+    email: 'tests@email.com',
+    password: '123456',
+    uid: '123',
+    name: 'Test',
+}
